ef0c66d382f2f82a979622c88a10dea9981a4408
RangNadh <ranganadh1509@gmail.com>
2022-07-12 15:54:45 +0530

    commit

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome';
-import Hello from './components/Hello'
+import Hello from './components/Hello';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      {/* <Greet />
-      <Welcome />  */}
-      <Hello />
+      <Message /> 
+      {/* <Greet name="Bruce" heroName="Batman">
+        <p>This is a children props</p>
+      </Greet>
+      <Greet name='Clark' heroName="Superman"/> 
+      <Greet name='Diana' heroName="Wonder Women"/>
+      <Welcome name="Bruce" heroName="Batman"></Welcome> 
+      <Hello /> */}
     </div>
   );
 }
