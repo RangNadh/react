69895f34f607d85700a1bf0ad5e365e5bcb830a0
RangNadh <ranganadh1509@gmail.com>
2022-07-14 15:46:02 +0530

    commit

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Person from './Person'
 
 function NameList() {
-    //const names = ['Bruce', 'Clark', 'Diana']
-    // const NameList = names.map(name => <h2>{name}</h2>)
+    const names = ['Bruce', 'Clark', 'Diana', 'Bruce']
+    const nameList = names.map((name, index) => <h2 key={name}>{index} {name}</h2>)
     const persons = [
         {
             id: 1,
@@ -25,7 +25,7 @@ function NameList() {
         }
     ]
 
-    const personList = persons.map(person => <Person person = {person}/>)
+    // const personList = persons.map(person => <Person key={person.id} person = {person}/>)
   return (
     <div>
         {/* This is the common approach */}
@@ -40,7 +40,9 @@ function NameList() {
             names.map(name => <h2>{name}</h2>)
         } */}
 
-        {personList}
+        {/* {personList} */}
+
+        {nameList}
 
     </div>
   )
